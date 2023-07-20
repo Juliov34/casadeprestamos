971b2987e11926fa0793682d14372a9d3c1f9fd2
FranscicoACA6 <amoselchidori@gmail.com>
2023-07-20 01:23:56 -0600

    Modulo 3 terminado

---
+++ modulo3/scripts.js
@@ -0,0 +1,5 @@
+document.getElementById("submitButton").addEventListener("click", function () {
+    // Mostrar la tabla y el menú al hacer clic en el botón
+    document.getElementById("tableContainer").style.display = "block";
+    document.getElementById("menuContainer").style.display = "block";
+  });
